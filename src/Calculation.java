de9e536e849423a5c2aecd0158639c3a204cd348
Seraphim97 <savichevserafim@gmail.com>
2023-02-27 16:17:18 +0200

    Calculation class fixed

--- src/Calculation.java
+++ src/Calculation.java
@@ -7,19 +7,35 @@ public class Calculation {
         System.out.println(myArray.length);
         Random rd = new Random();
         int i;
-        for (i = 0; i < LENGTH; i++); {
-            System.out.println("This field № " + i);
+        for (i = 0; i < LENGTH; i++) {
+            myArray[i] = rd.nextInt();
+            System.out.println(i + " " +myArray[i]);
 
         }
 
 
 
         //3.2
-        int[] myArrayTwo = new int[10];
-        for(i = 0; i < myArrayTwo.length; i++); {
+        int[] myArrayTwo = {1,2,3,4,5,6,7,8};
+        for(i = 0; i < myArrayTwo.length; i++) {
+            System.out.println(myArrayTwo[i]);
 
-            System.out.println("This field " + i);
         }
+
+        int[] myArrayThree = {1,2,3,4,5,6,7,8,9,10};
+        int a = 0;
+        for (i = 0; i < myArrayTwo.length; i++) {
+            if (myArrayTwo[i] % 2 == 0) {
+                myArrayThree[a] = myArrayTwo[i];
+                a++;
+            }
+        }
+        int b = 0;
+        while (b < myArrayThree.length) {
+            System.out.println(b + " " +myArrayThree[a]);
+            b++;
+        }
+
 
 
     }
